c28e96ef9d070cf11e6faeb2616dad8b78c247ef
Matthias L. Jugel <leo@thinkberg.com>
2019-12-20 17:55:03 +0100

    add some debugging

--- service-check-anchors.sh
+++ service-check-anchors.sh
@@ -4,12 +4,19 @@ echo "waiting for ${WAIT}s before doing anchor verification..."
 sleep $WAIT
 errors=0
 while read -r line; do
+  [ "$LOGLEVEL" == "DEBUG" ] && \
+    curl --silent -d "$line" https://verify.${UBIRCH_ENV:-dev}.ubirch.com/api/upp/verify/record | jq
   echo -n "checking \"${line}\": "
   anchors=$(curl --silent -d "$line" https://verify.${UBIRCH_ENV:-dev}.ubirch.com/api/upp/verify/record | \
     jq -r '(.anchors.upper_blockchains | length)  + (.anchors.lower_blockchains | length)')
-  echo "${anchors} anchors found"
-  if [ ${anchors} -lt 2 ]; then
+  if [ "$?" != "0" ]; then
+    echo "error checking hash"
     errors=$((errors + 1))
+  else
+    echo "${anchors} anchors found"
+    if [ ${anchors} -lt 2 ]; then
+      errors=$((errors + 1))
+    fi
   fi
 done < hashes.txt
 if [ ${errors} -gt 0 ]; then
